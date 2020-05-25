01089ea4dcaafb7123b66ce0a87a3cc4bcd4e218
kaii <kxiang21@gsb.columbia.edu>
2020-05-25 16:43:48 +0800

    setup

---
+++ __init__.py
@@ -0,0 +1,10 @@
+print(
+"""
+Welcome to Kate version 2.0.1 (2020-05-25)
+ 
+ * Documentation:   https://github.com/kaiCbs/KatePublic
+ * Support:         kx2153@gsb.columbia.edu
+
+Copyright (C) 2020 Kai Xiang
+"""
+)
